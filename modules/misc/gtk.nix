e29c9fb4724f0f90cca5f016349493b883c2fac1
chn <chn@chn.moe>
2025-09-25 14:25:52 +0800

    force set gtk2 config

--- modules/misc/gtk.nix
+++ modules/misc/gtk.nix
@@ -267,9 +267,10 @@ in {
       cfg.cursorTheme
     ];
 
-    home.file.${cfg2.configLocation}.text =
+    home.file.${cfg2.configLocation} = { text =
       concatMapStrings (l: l + "\n") (mapAttrsToList formatGtk2Option gtkIni)
       + cfg2.extraConfig + "\n";
+      force = true; };
 
     home.sessionVariables.GTK2_RC_FILES = cfg2.configLocation;
 
